b40ad063b18ada00275ec6e0f9d807017831fd46
Evomatic <evantrujillo30@gmail.com>
2024-01-14 10:30:19 +0900

    Format files

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,23 +1,23 @@
 export default {
   printWidth: 100,
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'es5',
-    useTabs: true,
-    astroAllowShorthand: false,
-  plugins: ["prettier-plugin-astro"],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: true,
+  astroAllowShorthand: false,
+  plugins: ['prettier-plugin-astro'],
   overrides: [
     {
-			files: ['.*', '*.json', '*.md', '*.yml'],
-			options: {
-				useTabs: false,
-			},
-		},
-    {
-      files: "*.astro",
+      files: ['.*', '*.json', '*.md', '*.yml'],
       options: {
-        parser: "astro",
+        useTabs: false,
+      },
+    },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
       },
     },
   ],
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import htmx from 'astro-htmx'
+import htmx from 'astro-htmx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [htmx(), tailwind()],
+	integrations: [htmx(), tailwind()],
 });
