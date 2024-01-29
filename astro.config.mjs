7f23920f5b6fb8dbd1f68bbe1976125d26af5b0b
Evan Trujillo <evantrujillo30@gmail.com>
2024-01-29 18:16:01 +0900

    Add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [htmx()],
+  integrations: [htmx(), icon()]
 });
