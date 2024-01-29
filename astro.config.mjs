57e0450fda232b97947bacf67061f51f2aa91ed4
Evan <evantrujillo30@gmail.com>
2024-01-29 21:57:47 +0900

    Add custom path for icon location

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [htmx(), icon()]
+	integrations: [
+		htmx(),
+		icon({
+			iconDir: 'src/images/icons',
+		}),
+	],
 });
