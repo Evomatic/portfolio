a4f3fea7007c60a693b9b8bc5d504a0520f23a1c
evomatic <evantrujillo30@gmail.com>
2024-03-22 17:34:31 +0900

    WIP: Add api for sending emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
 import icon from 'astro-icon';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		htmx(),
-		icon({
-			iconDir: 'src/images/icons',
-		}),
-	],
+  integrations: [htmx(), icon({
+    iconDir: 'src/images/icons'
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
---
+++ src/pages/api/email.json.ts
@@ -0,0 +1,29 @@
+import type { APIRoute } from 'astro';
+import { Resend } from 'resend';
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const GET: APIRoute = async ({ params, request }) => {
+	const send = await resend.emails.send({
+		from: 'evantrujillo30@gmail.com',
+		to: 'evantrujillo30@gmail.com',
+		subject: 'test subject',
+		html: '<p>Sample email sent</p>',
+		text: 'Hi there',
+	});
+
+	// try {
+	// 	return new Response(JSON.stringify({ message: send.data }));
+	// } catch (error) {
+	// 	return new Response(JSON.stringify({ message: send.error }));
+	// }
+
+	if (send.data) {
+		return new Response(JSON.stringify({ message: send.data }), { status: 200, statusText: 'OK' });
+	} else {
+		return new Response(JSON.stringify({ message: send.error }), {
+			status: 500,
+			statusText: 'Internal Server Error',
+		});
+	}
+};
