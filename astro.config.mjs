22743d3e1910c7d67bac9f0e1515eb2aea71d1a5
Evan Trujillo <evantrujillo30@gmail.com>
2024-03-25 22:07:36 +0900

    Change adapter to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
 import icon from 'astro-icon';
 
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [htmx(), icon({
-    iconDir: 'src/images/icons'
-  })],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+	integrations: [
+		htmx(),
+		icon({
+			iconDir: 'src/images/icons',
+		}),
+	],
+	output: 'static',
+	adapter: node({
+		mode: 'standalone',
+	}),
 });
