462385e2525ffa880f5e40e7b225cc5c5020c158
Evan <evan@touruhiosatominoMacBook-Air.local>
2024-06-16 17:58:49 +0900

    disable email api

--- src/pages/api/email.json.ts
+++ src/pages/api/email.json.ts
@@ -1,40 +1,40 @@
-import type { APIRoute } from 'astro';
-import { Resend } from 'resend';
+// import type { APIRoute } from 'astro';
+// import { Resend } from 'resend';
 
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
+// const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
-export const POST: APIRoute = async ({ request }) => {
-	const formData = await request.formData();
-	const name = formData.get('name');
-	const email = formData.get('email');
-	const message = formData.get('message');
+// export const POST: APIRoute = async ({ request }) => {
+// 	const formData = await request.formData();
+// 	const name = formData.get('name');
+// 	const email = formData.get('email');
+// 	const message = formData.get('message');
 
-	const send = await resend.emails.send({
-		from: "Evan's Portfolio <no-reply@evantrujillo.com>",
-		to: 'evantrujillo30@gmail.com',
-		subject: `New message from: ${name} (${email})`,
-		html: `<p>${message}</p>`,
-	});
+// 	const send = await resend.emails.send({
+// 		from: "Evan's Portfolio <no-reply@evantrujillo.com>",
+// 		to: 'evantrujillo30@gmail.com',
+// 		subject: `New message from: ${name} (${email})`,
+// 		html: `<p>${message}</p>`,
+// 	});
 
-	if (send.data) {
-		return new Response(
-			JSON.stringify({
-				message: send.data,
-			}),
-			{
-				status: 200,
-				statusText: 'Email successfully sent!',
-			}
-		);
-	} else {
-		return new Response(
-			JSON.stringify({
-				message: send.error,
-			}),
-			{
-				status: 500,
-				statusText: 'Internal Server Error',
-			}
-		);
-	}
-};
+// 	if (send.data) {
+// 		return new Response(
+// 			JSON.stringify({
+// 				message: send.data,
+// 			}),
+// 			{
+// 				status: 200,
+// 				statusText: 'Email successfully sent!',
+// 			}
+// 		);
+// 	} else {
+// 		return new Response(
+// 			JSON.stringify({
+// 				message: send.error,
+// 			}),
+// 			{
+// 				status: 500,
+// 				statusText: 'Internal Server Error',
+// 			}
+// 		);
+// 	}
+// };
